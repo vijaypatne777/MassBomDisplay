66e6dda49817b6f00e4c0d62b4862006a51f0e08
S0021828756 <sandip.sahoo@wipro.com>
2020-05-19 11:51:02 +0000

    Import content for version 1.0.5

---
+++ test/integration/opaTests.qunit.js
@@ -0,0 +1,1 @@
+QUnit.config.autostart=false;sap.ui.getCore().attachInit(function(){"use strict";sap.ui.require(["com/bom/sap/com/zbomapp/test/integration/AllJourneys"],function(){QUnit.start()})});
